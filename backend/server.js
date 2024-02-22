77c7649fd47791ca3613c0d695541aadf0274f20
vikkramstark <vikkram26stark@gmail.com>
2024-02-22 23:32:23 +0530

    Tweaked ports

--- backend/server.js
+++ backend/server.js
@@ -85,7 +85,7 @@ app.post("/waitlist", async (req,res) => {
 })  
 
 
-const PORT = 5000; 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;  
+app.listen(PORT, "0.0.0.0", () => {
     console.log("[SERVER STARTED] http://localhost:"+PORT)  
 }) 
