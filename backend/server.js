8d43fd1cdb043907c7ff2ba50cd6862e85d32d83
vikkramstark <vikkram26stark@gmail.com>
2024-03-04 22:51:23 +0530

    Fixed Cors Methods

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,7 @@ db.connect(err => {
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
     next();
 });
 
