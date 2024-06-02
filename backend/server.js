c92a927a7615ec9df1b3f8e8db9827a96d86c1e5
Vikkram <vikkram26stark@gmail.com>
2024-06-03 00:42:18 +0530

    Updates mysql2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express(); 
-const mysql = require('mysql');  
+const mysql = require('mysql2');   
 require("dotenv").config(); 
 // const bcrypt = require('bcrypt');  
 const cors = require('cors'); 
